d992f5e264bda8ee8b1192a029505e7e5ea9accc
Jawad <jawadhfarooqui@gmail.com>
2021-10-03 06:31:48 +0530

    Implemented bill total API end point

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,37 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.post("bill", "total") { req -> TotalBill in
+        let bill = try req.content.decode(Bill.self)
+        guard bill.amount > 0 else {
+            return TotalBill(amount: bill.amount, tipPercentage: bill.tipPercentage,
+                             tip: 0, total: 0)
+        }
+        guard bill.tipPercentage > 0 else {
+            return TotalBill(amount: bill.amount, tipPercentage: bill.tipPercentage,
+                             tip: 0, total: bill.amount)
+        }
+        var calculatedTip = bill.amount * bill.tipPercentage / 100.0
+        var roundedTip = Decimal()
+        NSDecimalRound(&roundedTip, &calculatedTip, 2, .plain)  //Rounding to 2 decimal places
+        
+        let calculatedTotal = bill.amount + roundedTip
+        
+        let total = TotalBill(amount: bill.amount, tipPercentage: bill.tipPercentage,
+                              tip: roundedTip, total: calculatedTotal)
+        return total
+    }
+}
+
+struct Bill: Content {
+    let amount: Decimal
+    let tipPercentage: Decimal
+}
+
+struct TotalBill: Content {
+    let amount: Decimal
+    let tipPercentage: Decimal
+    let tip: Decimal
+    let total: Decimal
 }

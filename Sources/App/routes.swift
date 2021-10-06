4e19098ed271a93004cb94f278583ba4783f838b
Jawad <jawadhfarooqui@gmail.com>
2021-10-06 14:29:35 +0530

    Added leaf template implementation

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,18 +9,20 @@ func routes(_ app: Application) throws {
         return "Hello, world!"
     }
     
-    app.post("bill", "total") { req -> TotalBill in
+    app.post("bill", "total") { req throws -> TotalBill in
         let bill = try req.content.decode(Bill.self)
-        guard bill.amount > 0 else {
-            return TotalBill(amount: bill.amount, tipPercentage: bill.tipPercentage,
-                             tip: 0, total: 0)
+        //Validate Tip
+        let tipRange: Range<Decimal> = 0.0..<100.0
+        guard tipRange.contains(bill.tipPercentage) else {
+            throw Abort(.forbidden, reason: "Invalid tip percentage")
         }
-        guard bill.tipPercentage > 0 else {
-            return TotalBill(amount: bill.amount, tipPercentage: bill.tipPercentage,
-                             tip: 0, total: bill.amount)
+        //Validate Amount
+        guard bill.amount >= 0 else {
+            throw Abort(.forbidden, reason: "Invalid amount")
         }
+        //Calculate the tip based on the inputs provided
         var calculatedTip = bill.amount * bill.tipPercentage / 100.0
-        
+    
         //Rounding to 2 decimal places
         var roundedTip = Decimal()
         NSDecimalRound(&roundedTip, &calculatedTip, 2, .plain)
@@ -31,6 +33,27 @@ func routes(_ app: Application) throws {
                               tip: roundedTip, total: calculatedTotal)
         return total
     }
+    
+    app.get("bill") { req -> EventLoopFuture<View> in
+        let bill = BillData(amount: 0, tipPercentage: 15.0, tip: 0, total: 0)
+        return req.view.render("bill", bill)
+    }
+    
+    app.post("bill") { req -> EventLoopFuture<View> in
+        let billForm = try req.content.decode(BillForm.self)
+        let tipRange = 0.0..<100.0
+        guard tipRange.contains(billForm.tipPercentage) else {
+            throw Abort(.forbidden, reason: "Invalid tip percentage")
+        }
+        guard billForm.amount >= 0 else {
+            throw Abort(.forbidden, reason: "Invalid amount")
+        }
+        let calculatedTip = billForm.amount * billForm.tipPercentage / 100.0
+        let calculatedTotal = billForm.amount + calculatedTip
+        let bill = BillData(amount: billForm.amount, tipPercentage: billForm.tipPercentage, tip: calculatedTip, total: calculatedTotal)
+        return req.view.render("bill", bill)
+    }
+
 }
 
 struct Bill: Content {
@@ -43,4 +66,17 @@ struct TotalBill: Content {
     let tipPercentage: Decimal
     let tip: Decimal
     let total: Decimal
+}
+
+
+struct BillForm: Decodable {
+    let amount: Double
+    let tipPercentage: Double
+}
+
+struct BillData: Encodable {
+    let amount: Double
+    let tipPercentage: Double
+    let tip: Double
+    let total: Double
 }
